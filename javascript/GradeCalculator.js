b4a21bfb99bf7aabd127d290b50dea720c9cda6c
rb347 <101583505+rb347@users.noreply.github.com>
2022-03-23 14:13:13 -0500

    Update GradeCalculator.js

--- javascript/GradeCalculator.js
+++ javascript/GradeCalculator.js
@@ -1,5 +1,3 @@
-
-
 // let AddressBook = {
 //   book: [],
 //   add: function(address) {
@@ -21,32 +19,43 @@
 //         console.log(address);
 
 let gradecalculator = {
-   grade: [],
-
-   console.log(calculateGrade(marks));
-
-   function calculateGrade(marks) {
-     let average = calculateAverage(marks);
-
-     if (average == null) return 'No Grades';
-     if (average < 60) return 'F';
-     if (average < 70) return 'D';
-     if (average < 80) return 'C';
-     if (average < 90) return 'B';
-     if (average <= 100) return 'A';
+  grades: [],
+  addGrade: function (grade) {
+    if (isNaN(grade)) {
+      console.log("Bad Value: " + grade);
+    } else {
+      this.grades.push(grade);
     }
-
-    function calculateAverage(array) {
-      // If student has no grades, return null
-      if (array.length == 0) return null;
-
-      // calculate average for student since he has grades
-      let sum = 0;
-      for (let value of array) {
-        sum += value;
-      }
-
-      return sum / array.length;
+  },
+  showGrade: function () {
+    console.log("Class Average: " + this.calculateGrade());
+  },
+  calculateGrade: function () {
+    let average = this.calculateAverage();
+    if (average == null) return "No Grades";
+    if (average < 60) return "F";
+    if (average < 70) return "D";
+    if (average < 80) return "C";
+    if (average < 90) return "B";
+    if (average <= 100) return "A";
+  },
+  calculateAverage: function () {
+    let gradebook = this.grades;
+    // If student has no grades, return null
+    if (gradebook.length == 0) return null;
+    // calculate average for student since he has grades
+    let sum = 0;
+    for (let value of gradebook) {
+      sum += value;
     }
+    return sum / gradebook.length;
+  },
+};
 
-  }
+gradecalculator.addGrade("A");
+gradecalculator.addGrade(10);
+gradecalculator.addGrade(56);
+gradecalculator.addGrade(99);
+gradecalculator.addGrade(105);
+
+gradecalculator.showGrade();
