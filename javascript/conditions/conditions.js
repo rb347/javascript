9ebbdbf2ea775e1253a0c13bb914aea57dcca213
rb347 <101583505+rb347@users.noreply.github.com>
2022-04-05 15:46:39 -0500

    ifelse

--- javascript/conditions/conditions.js
+++ javascript/conditions/conditions.js
@@ -1,18 +1,25 @@
+document.getElementById("btn_check").addEventListener("click", check_number);
 document
-  .getElementById("btn_check")
-  .addEventListener("click", check_word);
+        .getElementById("number")
+        .addEventListener("keyup", function (event) {
+          if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("btn_check").click();
 
-
-	function check_word() {
-		let word = document.getElementById('word').value;
-		if (word > '') {
-				let result = 'Unknown';
-				if (word == 'red') {
-					result = 'color';
-				}
-				if (word == 'apple') {
-					result = 'fruit';
-				}
-				document.getElementById('results').innerHTML += result + " : " + word + "<br/>";
-		}
-	}
+function check_number() {
+  let number = document.getElementById("number").value;
+  if (number > "") {
+    let result = "Unknown";
+    if (word === "1") {
+      result = "1 is neither prime nor composite number.";
+    } else if (number > 1) {
+      for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+          isPrime = false;
+        }
+        document.getElementById("results").innerHTML +=
+          result + " : " + number + "<br/>";
+      }
+    }
+  }
+}
