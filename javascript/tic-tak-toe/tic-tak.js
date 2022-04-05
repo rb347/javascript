81a179d97fdb6f5176e129359df09c4b7caf9bf7
rb347 <101583505+rb347@users.noreply.github.com>
2022-04-05 08:59:56 -0500

    update

---
+++ javascript/tic-tak-toe/tic-tak.js
@@ -0,0 +1,18 @@
+let tikTak = {
+
+const boardGane = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const winCombo = [Top Row: 123, Mid Row: 456, Bot Row: 789, Fir Col: 147,sec col: 258, thi col: 369, Dia one: 159, Dia two: 357];
+
+let redPick = function(){
+document.getElementById("red_pick").innerHTML
+}
+
+let bluePick = function(){
+document.getElementById("blue_pick").innerHTML
+}
+
+document.getElementById("btn_red").addEventListener("click", tikTak.red_pick);
+
+document.getElementById("btn_blue").addEventListener("click", tikTak.blue_pick);
+}
