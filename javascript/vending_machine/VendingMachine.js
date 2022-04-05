3ae06fa0125835d94465ca657318b3a6d89928bd
rb347 <101583505+rb347@users.noreply.github.com>
2022-04-05 09:49:46 -0500

    vending update

--- javascript/vending_machine/VendingMachine.js
+++ javascript/vending_machine/VendingMachine.js
@@ -3,15 +3,15 @@
 let VendingMachine = {
   // Variables / Properties
   items: {
-    A1: { item_name: "Fruit Loops", price: 3.5 },
-    A2: { item_name: "Yogurt", price: 1.5 },
-    A3: { item_name: "Snickers", price: 1.75 },
-    B1: { item_name: "M&M's", price: 0.25 },
-    B2: { item_name: "Bubble Gum", price: 0.5 },
-    B3: { item_name: "Orange", price: 1.15 },
-    C1: { item_name: "Reeses Pieces", price: 1.0 },
-    C2: { item_name: "Oreo", price: 1.2 },
-    C3: { item_name: "Grapefruit", price: 2.5 },
+    A1: { item_name: "Fruit Loops", price: 3.5, quantity: 10 },
+    A2: { item_name: "Yogurt", price: 1.5, quantity: 10 },
+    A3: { item_name: "Snickers", price: 1.75, quantity: 10 },
+    B1: { item_name: "M&M's", price: 0.25, quantity: 10 },
+    B2: { item_name: "Bubble Gum", price: 0.5, quantity: 10 },
+    B3: { item_name: "Orange", price: 1.15, quantity: 10 },
+    C1: { item_name: "Reeses Pieces", price: 1.0, quantity: 10 },
+    C2: { item_name: "Oreo", price: 1.2, quantity: 10 },
+    C3: { item_name: "Grapefruit", price: 2.5, quantity: 10 },
   },
   money_in: 0,
 
@@ -22,6 +22,20 @@ let VendingMachine = {
       currency: "USD",
     });
     return dollarUSLocale.format(price);
+  },
+  check_inventory: function(){
+    let item = document.getElementById("item_selection").value;
+		for (selection in VendingMachine.items) {
+      if (selection === item) {
+        let item_quantity = VendingMachine.items[selection].quantity;
+        if (item_quantity >= 1){
+          return true;
+        } else {
+          return false;
+        }
+    
+      }
+    }
   },
   get_item: function () {
 		let item = document.getElementById("item_selection").value;
@@ -38,6 +52,7 @@ let VendingMachine = {
 			return;
         }
       }
+      this.check_inventory()
     }
     console.log("Item does not exist");
   },
@@ -79,6 +94,20 @@ document
 document
   .getElementById("btn_add_money")
   .addEventListener("click", VendingMachine.add_money);
+
+document
+  .getElementById("item_selection")
+  .addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("btn_get_item").click();
+
+document
+  .getElementById("money_in")
+  .addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("btn_add_money").click();
 
 // display the items
 VendingMachine.display_items();
